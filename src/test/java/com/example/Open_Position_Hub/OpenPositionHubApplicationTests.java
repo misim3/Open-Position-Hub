1af8a75e01e211df55c6f7d6d61073279cbdf47c
misim3 <sim000314@naver.com>
2025-01-12 16:55:04 +0900

    chore: 프로젝트 초기 설정

---
+++ src/test/java/com/example/Open_Position_Hub/OpenPositionHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Open_Position_Hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenPositionHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
