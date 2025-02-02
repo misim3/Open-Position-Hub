ce02f0b30ae490bc0da135dae1d6245b6dea656b
misim3 <sim000314@naver.com>
2025-02-02 12:12:44 +0900

    refactor: 앞으로 추구하고자 하는 리팩토링 방향 주석으로 작성

--- src/main/java/com/example/Open_Position_Hub/OpenPositionHubApplication.java
+++ src/main/java/com/example/Open_Position_Hub/OpenPositionHubApplication.java
@@ -8,6 +8,11 @@ public class OpenPositionHubApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OpenPositionHubApplication.class, args);
+
+		/*
+		큰 구조는 큰 컨테이너가 존재해서 각 모듈 호출
+		각 모듈은 스크래퍼, 셀렉터, 추출기, 변환기, 레포지토리.
+		 */
 	}
 
 }
