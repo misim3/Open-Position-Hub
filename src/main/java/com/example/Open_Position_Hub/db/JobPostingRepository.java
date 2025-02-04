898739299e9edfcc72097311081725472507bd68
misim3 <sim000314@naver.com>
2025-02-04 22:18:31 +0900

    refactor: jpa 활용한 레포지토리로 변경 및 메모리 저장공간을 사용하는 레포지토리 삭제

---
+++ src/main/java/com/example/Open_Position_Hub/db/JobPostingRepository.java
@@ -0,0 +1,7 @@
+package com.example.Open_Position_Hub.db;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JobPostingRepository extends JpaRepository<JobPostingEntity, Long> {
+
+}
