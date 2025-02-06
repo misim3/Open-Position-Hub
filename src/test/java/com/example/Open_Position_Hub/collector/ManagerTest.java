cae0358b9b101d7fe0f74f3b04f205054d5b72b0
misim3 <sim000314@naver.com>
2025-02-06 22:00:51 +0900

    test: 채용 공고 수집 기능 관리 클래스 테스트 구현 및 실행 결과 확인

---
+++ src/test/java/com/example/Open_Position_Hub/collector/ManagerTest.java
@@ -0,0 +1,41 @@
+package com.example.Open_Position_Hub.collector;
+
+import com.example.Open_Position_Hub.db.JobPostingEntity;
+import com.example.Open_Position_Hub.db.JobPostingRepository;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerTest {
+
+    @Autowired
+    private JobPostingRepository jobPostingRepository;
+
+    @Test
+    void process_greeting_typeA() {
+        String url = "https://teamdoeat.career.greetinghr.com";
+        Scraper scraper = new Scraper();
+        Extractor extractor = new Extractor(new CssSelector(), new JobDataExtractorSelenium());
+        Manager manager = new Manager(scraper, extractor, jobPostingRepository);
+
+        manager.process(url);
+
+        List<JobPostingEntity> jobPostingEntities = jobPostingRepository.findAll();
+        jobPostingEntities.forEach(System.out::println);
+    }
+
+    @Test
+    void process_greeting_typeB() {
+        String url = "https://www.doodlin.co.kr";
+        Scraper scraper = new Scraper();
+        Extractor extractor = new Extractor(new CssSelector(), new JobDataExtractorSelenium());
+        Manager manager = new Manager(scraper, extractor, jobPostingRepository);
+
+        manager.process(url);
+
+        List<JobPostingEntity> jobPostingEntities = jobPostingRepository.findAll();
+        jobPostingEntities.forEach(System.out::println);
+    }
+}
