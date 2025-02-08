c43fa5f2755c3882926687c46216045a39a28974
misim3 <sim000314@naver.com>
2025-02-08 21:24:00 +0900

    refactor: 회사 엔티티 클래스에 채용 플랫폼과 채용 url 필드 추가

--- src/main/java/com/example/Open_Position_Hub/db/CompanyEntity.java
+++ src/main/java/com/example/Open_Position_Hub/db/CompanyEntity.java
@@ -6,21 +6,35 @@ import jakarta.persistence.Entity;
 public class CompanyEntity extends BaseEntity {
 
     private String name;
+    private String recruitmentPlatform;
+    private String recruitmentUrl;
 
     protected CompanyEntity() {}
 
-    public CompanyEntity(String name) {
+    public CompanyEntity(String name, String recruitmentPlatform, String recruitmentUrl) {
         this.name = name;
+        this.recruitmentPlatform = recruitmentPlatform;
+        this.recruitmentUrl = recruitmentUrl;
     }
 
     public String getName() {
         return name;
     }
 
+    public String getRecruitmentPlatform() {
+        return recruitmentPlatform;
+    }
+
+    public String getRecruitmentUrl() {
+        return recruitmentUrl;
+    }
+
     @Override
     public String toString() {
-        return "Company{" +
+        return "CompanyEntity{" +
             "name='" + name + '\'' +
+            ", recruitmentPlatform='" + recruitmentPlatform + '\'' +
+            ", recruitmentUrl='" + recruitmentUrl + '\'' +
             '}';
     }
 }
