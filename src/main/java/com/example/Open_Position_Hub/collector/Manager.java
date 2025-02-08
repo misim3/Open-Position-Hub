2102b83723b3531a08643ae7435c41154851cd34
misim3 <sim000314@naver.com>
2025-02-08 21:48:29 +0900

    refactor: db에서 읽어온 회사 정보를 바탕으로 채용 공고 수집 기능 동작하도록 수정

--- src/main/java/com/example/Open_Position_Hub/collector/Manager.java
+++ src/main/java/com/example/Open_Position_Hub/collector/Manager.java
@@ -1,5 +1,7 @@
 package com.example.Open_Position_Hub.collector;
 
+import com.example.Open_Position_Hub.db.CompanyEntity;
+import com.example.Open_Position_Hub.db.CompanyRepository;
 import com.example.Open_Position_Hub.db.JobPostingEntity;
 import com.example.Open_Position_Hub.db.JobPostingRepository;
 import java.io.IOException;
@@ -15,30 +17,41 @@ public class Manager {
     private final Scraper scraper;
     private final Extractor extractor;
     private final JobPostingRepository jobPostingRepository;
+    private final CompanyRepository companyRepository;
 
-    public Manager(Scraper scraper, Extractor extractor, @Autowired JobPostingRepository jobPostingRepository) {
+    public Manager(Scraper scraper, Extractor extractor,
+        @Autowired JobPostingRepository jobPostingRepository,
+        @Autowired CompanyRepository companyRepository) {
         this.scraper = scraper;
         this.extractor = extractor;
         this.jobPostingRepository = jobPostingRepository;
+        this.companyRepository = companyRepository;
     }
 
     @Scheduled(fixedRate = 86400000) // 24시간 마다 실행
     public void process() {
         System.out.println("Processing...");
-        List<JobPostingEntity> jobPostingEntities = processJobScraping("https://teamdoeat.career.greetinghr.com");
-        saveJobPostings(jobPostingEntities);
+        List<CompanyEntity> companies = companyRepository.findAll();
+        companies.forEach(company -> saveJobPostings(processJobScraping(company)));
         System.out.println("Done!");
     }
 
-    private List<JobPostingEntity> processJobScraping(String url) {
+    private List<JobPostingEntity> processJobScraping(CompanyEntity company) {
 
         try {
-            Document doc = scraper.fetchHtml(url);
-            return extractor.extractGreeting2(doc, url);
+            Document doc = scraper.fetchHtml(company.getRecruitmentUrl());
+            if (company.getRecruitmentPlatform().equals("greeting")) {
+                return extractor.extractGreeting2(doc, company.getRecruitmentUrl(),
+                    company.getId());
+            } else {
+                System.out.println("this recruitment platform(" + company.getRecruitmentPlatform()
+                    + ") is not supported now.");
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
+        return List.of();
     }
 
     private void saveJobPostings(List<JobPostingEntity> jobPostingEntities) {
