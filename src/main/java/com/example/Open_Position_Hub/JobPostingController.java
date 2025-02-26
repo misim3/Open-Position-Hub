7d8ad5b36aad1b9d98766b0e06028162a37a79a9
misim3 <sim000314@naver.com>
2025-02-26 21:38:26 +0900

    feat: 공고 검색 컨트롤러 클래스 구현

---
+++ src/main/java/com/example/Open_Position_Hub/JobPostingController.java
@@ -0,0 +1,42 @@
+package com.example.Open_Position_Hub;
+
+import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JobPostingController {
+
+    private JobPostingService jobPostingService;
+
+    public JobPostingController(JobPostingService jobPostingService) {
+        this.jobPostingService = jobPostingService;
+    }
+
+    @GetMapping("/jobs")
+    public ResponseEntity<List<JobPosting>> getJobPostings() {
+
+        List<JobPosting> jobPostings = jobPostingService.getAllJobPostings();
+
+        return new ResponseEntity<>(jobPostings, HttpStatus.OK);
+    }
+
+    @GetMapping("/jobs")
+    public ResponseEntity<List<JobPosting>> getJobPostingsByTitle(@RequestParam String title) {
+
+        List<JobPosting> jobPostings = jobPostingService.getJobPostingsByTitle(title);
+
+        return new ResponseEntity<>(jobPostings, HttpStatus.OK);
+    }
+
+    @GetMapping("/jobs")
+    public ResponseEntity<List<JobPosting>> getJobPostingsByCompany(@RequestParam String companyName) {
+
+        List<JobPosting> jobPostings = jobPostingService.getJobPostingsByCompanyName(companyName);
+
+        return new ResponseEntity<>(jobPostings, HttpStatus.OK);
+    }
+}
