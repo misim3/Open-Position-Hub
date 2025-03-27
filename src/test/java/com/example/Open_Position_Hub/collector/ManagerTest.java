7ca06cffc606d95880e440c91618a9a6748a8365
misim3 <sim000314@naver.com>
2025-03-27 21:19:12 +0900

    feat: 매니저 테스트 실행 전, 후 db 삭제

--- src/test/java/com/example/Open_Position_Hub/collector/ManagerTest.java
+++ src/test/java/com/example/Open_Position_Hub/collector/ManagerTest.java
@@ -4,6 +4,7 @@ import com.example.Open_Position_Hub.db.CompanyEntity;
 import com.example.Open_Position_Hub.db.CompanyRepository;
 import com.example.Open_Position_Hub.db.JobPostingRepository;
 import java.util.List;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,28 +22,28 @@ class ManagerTest {
     @Autowired
     private Manager manager;
 
-    @BeforeEach
-    void setUp() {
-        jobPostingRepository.deleteAll();
-        companyRepository.deleteAll();
-    }
-
     private CompanyEntity doeat() {
-        return new CompanyEntity("doeat", "greeting", "https://teamdoeat.career.greetinghr.com");
+        return new CompanyEntity("doeat", "greeting", "https://teamdoeat.career.greetinghr.com/home#323ea93b-ce52-45c9-bbbf-0b85ad135508");
     }
 
     private CompanyEntity doodlin() {
-        return new CompanyEntity("doodlin", "greeting", "https://www.doodlin.co.kr");
+        return new CompanyEntity("doodlin", "greeting", "https://www.doodlin.co.kr/career#3276397a-a988-4ca5-ab47-9aa05e9cce30");
     }
 
     @Test
     void test() {
+
+        companyRepository.deleteAll();
+        jobPostingRepository.deleteAll();
 
         companyRepository.saveAll(List.of(doeat(), doodlin()));
 
         manager.process();
 
         jobPostingRepository.findAll().forEach(System.out::println);
+
+        companyRepository.deleteAll();
+        jobPostingRepository.deleteAll();
 
     }
 }
