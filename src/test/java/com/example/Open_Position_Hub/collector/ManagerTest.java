34ec29e0a42cf7e9da032a5bbc0640ea768a816c
misim3 <sim000314@naver.com>
2025-08-18 22:18:41 +0900

    feat: 채용 url 변경

--- src/test/java/com/example/Open_Position_Hub/collector/ManagerTest.java
+++ src/test/java/com/example/Open_Position_Hub/collector/ManagerTest.java
@@ -27,11 +27,11 @@ class ManagerTest {
     private Environment env;
 
     private CompanyEntity doeat() {
-        return new CompanyEntity("doeat", "그리팅", "https://teamdoeat.career.greetinghr.com/home#323ea93b-ce52-45c9-bbbf-0b85ad135508");
+        return new CompanyEntity("doeat", "그리팅", "https://teamdoeat.career.greetinghr.com/ko/jobposting#323ea93b-ce52-45c9-bbbf-0b85ad135508");
     }
 
     private CompanyEntity doodlin() {
-        return new CompanyEntity("doodlin", "그리팅", "https://www.doodlin.co.kr/career#3276397a-a988-4ca5-ab47-9aa05e9cce30");
+        return new CompanyEntity("doodlin", "그리팅", "https://www.doodlin.co.kr/ko/career#3276397a-a988-4ca5-ab47-9aa05e9cce30");
     }
 
     @Test
