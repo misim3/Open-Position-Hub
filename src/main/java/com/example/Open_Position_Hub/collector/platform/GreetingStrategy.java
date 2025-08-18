e6a4fe23dba6f2dca929aad360745a6e7e95bb35
misim3 <sim000314@naver.com>
2025-08-18 21:55:55 +0900

    feat: 플랫폼별 수집 전략 적용

--- src/main/java/com/example/Open_Position_Hub/collector/platform/GreetingStrategy.java
+++ src/main/java/com/example/Open_Position_Hub/collector/platform/GreetingStrategy.java
@@ -1,8 +1,11 @@
 package com.example.Open_Position_Hub.collector.platform;
 
+import com.example.Open_Position_Hub.collector.detect.DefaultDetectorRegistry;
+import com.example.Open_Position_Hub.collector.parser.ParserRegistry;
 import com.example.Open_Position_Hub.db.CompanyEntity;
 import com.example.Open_Position_Hub.db.JobPostingEntity;
 import java.util.List;
+import java.util.Optional;
 import org.jsoup.nodes.Document;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +13,14 @@ import org.springframework.stereotype.Component;
 public class GreetingStrategy implements PlatformStrategy {
 
     private static final String key = "그리팅";
+    private final DefaultDetectorRegistry defaultDetectorRegistry;
+    private final ParserRegistry parserRegistry;
+
+    public GreetingStrategy(DefaultDetectorRegistry defaultDetectorRegistry,
+        ParserRegistry parserRegistry) {
+        this.defaultDetectorRegistry = defaultDetectorRegistry;
+        this.parserRegistry = parserRegistry;
+    }
 
     @Override
     public boolean supports(String platform) {
@@ -18,6 +29,12 @@ public class GreetingStrategy implements PlatformStrategy {
 
     @Override
     public List<JobPostingEntity> scrape(Document doc, CompanyEntity company) {
+
+        Optional<String> layoutKey = defaultDetectorRegistry.detect(platformKey(), doc);
+
+        if (layoutKey.isPresent()) {
+            return parserRegistry.get(layoutKey.get()).parse(doc, company);
+        }
 
         return List.of();
     }
