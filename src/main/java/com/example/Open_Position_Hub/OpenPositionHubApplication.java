35e42a99c39ac124c34f334d877b2d82c08fa1a4
misim3 <sim000314@naver.com>
2025-09-17 23:00:47 +0900

    style: 불필요한 import 삭제

--- src/main/java/com/example/Open_Position_Hub/OpenPositionHubApplication.java
+++ src/main/java/com/example/Open_Position_Hub/OpenPositionHubApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
