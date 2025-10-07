3851943c5bc001dfc1e6a24e7028d7b8dfd4bbb7
misim3 <sim000314@naver.com>
2025-10-07 16:13:43 +0900

    feat: 사용하지 않는 메서드 삭제

--- src/main/java/com/example/Open_Position_Hub/db/JobPostingRepository.java
+++ src/main/java/com/example/Open_Position_Hub/db/JobPostingRepository.java
@@ -1,24 +1,11 @@
 package com.example.Open_Position_Hub.db;
 
-import java.util.Collection;
 import java.util.List;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface JobPostingRepository extends JpaRepository<JobPostingEntity, Long> {
-
-    Page<JobPostingEntity> findByTitleLike(String title, Pageable pageable);
-
-    Page<JobPostingEntity> findByTitleIn(Collection<String> titles, Pageable pageable);
-
-    @Query("select distinct j.title from JobPostingEntity j where j.title is not null ")
-    List<String> findDistinctTitles();
-
-    @Query("select distinct j.companyId from JobPostingEntity j where j.companyId is not null ")
-    List<Long> findDistinctCompanyIds();
 
     List<JobPostingEntity> findByCompanyId(Long companyId);
 
